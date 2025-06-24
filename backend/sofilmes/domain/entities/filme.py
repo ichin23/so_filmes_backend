5faefd2248e8c7ac93b9036433d5056efe31371b
ichin23 <pedroferreiramanoel@gmail.com>
2025-06-24 16:47:49 -0300

    first commit

---
+++ backend/sofilmes/domain/entities/filme.py
@@ -0,0 +1,11 @@
+class Filme:
+    def __init__(self, id: str, titulo: str, tituloOriginal:str, capa: str, descricao: str, avaliacao: float, ano: int, generos: list[str], diretor: str):
+        self.id = id
+        self.titulo = titulo
+        self.tituloOriginal = tituloOriginal
+        self.case = capa
+        self.descricao = descricao
+        self.avaliacao=avaliacao
+        self.ano = ano
+        self.generos = generos
+        self.diretor = diretor
---
+++ backend/sofilmes/domain/entities/user.py
@@ -0,0 +1,7 @@
+class Usuario:
+    def __init__(self, id:str, nome: str, username: str, email: Email, senha: Senha):
+        self.id = id
+        self.nome = nome
+        self.username = username
+        self.email = email
+        self.senha = senha
